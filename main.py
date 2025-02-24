55dd70795d21a9a2353fc821462d306eea3337d7
Michael-Kowata <kowata.michael@gmail.com>
2025-02-24 11:36:12 -0800

    Added the eastern path area dialogue and changed the premise of the story a little  bit.

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def main():
     intersection(protag)
 
 def gate(protag):
-    print ("\nIn front of you stands a guard to the town which you were exhiled.")
-    print (f"Guard: Hello {protag.get_name()}, I cannot allow you back into town after your crimes. If you can redeem yourself the council will consider your crimes forgiven.\n")
+    print ("\nIn front of you stands a gate to dangerous dungeon.")
+    print (f"Dungeon Guard: Hello {protag.get_name()}, as punishment for your crimes you were sentenced to scout and map out the area. If you can redeem yourself the council will consider your crimes forgiven.\n")
 
     while (True):
         print ("-------------------------------------------------------")
@@ -26,7 +26,7 @@ def gate(protag):
         Choice = input("\nOption: ")
         
         if Choice == '1':
-            print ("You stole and ate the mayor's prized cookie collection of course! Now please leave!\n")
+            print ("You stole and ate the mayor's prized cookie collection of course! Now off you go!\n")
         elif Choice == '2':
             print ("Hey what do you think you are doing! (You lost 2 health!)\n")
             protag.take_damage(2)
@@ -34,9 +34,9 @@ def gate(protag):
                 sys.exit()
             print (f"Current Health: {protag.get_health()}")
         elif Choice == '3':
-            print ("There's a growing monster popoluation that is terrorizing the local farmers. Reduce their population and I shall let you back in.\n")
+            print ("There's a growing monster popoluation in this dungeon that is terrorizing the local farmers. Reduce their population and we will pardon your transgression; that is if you manage to even come out alive. Ha ha!\n")
         else:
-            print ("You left the gate.\n")
+            print ("You left the gate and walked into the dungeon.\n")
             break  
 
 def intersection(protag):
@@ -59,6 +59,7 @@ def intersection(protag):
             break
         elif Choice == '2':
             print ("You decided to check out the east path.\n")
+            east(protag)
             break
         elif Choice == '3':
             print ("You went back to the gate.\n")
@@ -67,36 +68,56 @@ def intersection(protag):
             print ("You chose to walk the western path.\n")
             break
         elif Choice == '5':
-            print ("You read the sign.\n")
-            print ("North path -> Moonshine Valley")
-            print ("East path -> Grave of the Dragonflies")
-            print ("South path -> Cookie Dough Kingdom")
-            print ("West path -> Lake of Lost Souls\n")
+            print ("You read the sign. It's worn out so it's hard to decipher what it is saying.\n")
+            print ("North path -> De  h Val  y")
+            print ("East path -> Gr v  of H ro s")
+            print ("South path -> S im  Ki gd m")
+            print ("West path -> S litu e\n")
         else:
             print ("Invalid option.\n")
 
 def north(protag):
-    print ("Walking across Moonshine Valley. You see a shiny glowing rock. What do you do?")
+    print ("Walking across the northern path. You see a shiny glowing rock. What do you do?")
     
     while (True):
         print ("-------------------------------------------------------")
         print ("Options:")
         print ("1: Pick it up")
         print ("2: Kick it")
-        print ("3: Leave")
         print ("-------------------------------------------------------")
         Choice = input("\nOption: ")
     
         if Choice == '1':
-            print ("You picked up the rock. The mystical rock has been added to your inventory.\n")
+            print ("You picked up the rock. A wild spirit attacks!.\n")
+            monster = Monster("spirit")
             break
         elif Choice == '2':
-            print ("You kicked the rock. A wild spirit appears from the rock!")
+            print ("You kicked the rock. A wild spirit appears from the rock! ")
             monster = Monster("spirit")
             #Encounter function 
             break
-        elif Choice == '3':
-            print ("You decided to leave.\n")
+        else:
+            print ("Invalid option.\n")
+
+def east(protag):
+    print ("Walking across the eastern path. You hear a bunch of slime roaming around. What do you do?")
+    
+    while (True):
+        print ("-------------------------------------------------------")
+        print ("Options:")
+        print ("1: Charge at them!")
+        print ("2: Try and sneak by")
+        print ("-------------------------------------------------------")
+        Choice = input("\nOption: ")
+    
+        if Choice == '1':
+            print ("You charge at the slimes. In the process you accidently slip and bump into one of them.\n")
+            monster = Monster("slime")
+            break
+        elif Choice == '2':
+            print ("You try sneaking past the slimes, but they heard your footsteps! ")
+            monster = Monster("slime")
+            #Encounter function 
             break
         else:
             print ("Invalid option.\n")
